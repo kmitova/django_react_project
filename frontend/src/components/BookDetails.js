333babaef0c3e3e0b6dcd4bf4b167494a28f7b11
kmitova <96522051+kmitova@users.noreply.github.com>
2023-01-15 13:38:07 +0200

    add a review on the frontend

--- frontend/src/components/BookDetails.js
+++ frontend/src/components/BookDetails.js
@@ -8,6 +8,10 @@ import {URL} from "../utils/url";
 const BookDetails = () => {
     const {id} = useParams()
     const [book, setBook] = useState([]);
+    const [bookIsRead, setBooksIsRead] = useState(false)
+
+    const [content, setContent] = useState('')
+    const [rating, setRating] = useState('')
 
     useEffect(() => {
     const fetchBook = async () => {
@@ -29,7 +33,7 @@ const BookDetails = () => {
         .catch(console.error);
   }, [id]);
 
-        const [bookIsRead, setBooksIsRead] = useState(false)
+
 
 
     const changeStatus = async (e, book) => {
@@ -60,6 +64,36 @@ const BookDetails = () => {
 
     }
 
+    const handleSubmit = async (e) => {
+            e.preventDefault()
+
+        let token = getAccessToken();
+        let data = {
+            rating: rating,
+            content: content,
+            book: book.id
+
+        }
+
+        console.log(data)
+        await axios.post(`${URL}api_books/add_review/`,
+            data, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .then((result) => {
+                // console.log(category)
+                console.log(result.data);
+            })
+            .catch((error) => {
+                // console.log(category)
+                console.log(error);
+            })
+    }
+
     return (
         <div>
             <h1>Book details page</h1>
@@ -72,6 +106,25 @@ const BookDetails = () => {
                            onChange={(e) => changeStatus(e, book)}/> :
                     <input type="checkbox" name="is_read" onChange={(e) => changeStatus(e, book)}/>}
             </form>
+            <form action="" onSubmit={handleSubmit}>
+                <h3>Review this book:</h3>
+                <div>
+                    <label htmlFor="">Rating</label>
+                    <select name="" id="" value={rating} onChange={(e) => setRating(e.target.value)}>
+                          <option value="5">5 stars</option>
+                            <option value="4">4 stars</option>
+                            <option value="3">3 stars</option>
+                            <option value="2">2 stars</option>
+                            <option value="1">1 star</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="">Review:</label>
+                    <textarea name="" id="" cols="30" rows="10" value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+                </div>
+                <button>Post Review</button>
+            </form>
+            {/* show other review if any */}
         </div>
 
     )

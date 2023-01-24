9a5436f2defe0206aa386d0a1c6498ac7b7dc0b4
kmitova <96522051+kmitova@users.noreply.github.com>
2023-01-24 17:16:34 +0200

    search form redesign

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,17 +1,45 @@
-import { useContext } from "react";
+import {useContext, useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
+import SearchBook from "./SearchBook";
+import getAccessToken from "../utils/getToken";
+import axios from "axios";
+import {URL} from "../utils/url";
 
 const Navbar = () => {
   const { user, logoutUser } = useContext(AuthContext);
   console.log(user)
+    const [books, setBooks] = useState([])
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            let token = getAccessToken();
+            let result = await axios.get(`${URL}api_books/books/`, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer  ${token}`
+                }
+            });
+            if (result.status === 200) {
+                const books = result.data
+                console.log(books)
+                setBooks(books)
+            }
+        }
+        fetchData()
+            .catch(console.error)
+    }, [])
   return (
     <nav>
       <div>
         <h1>App Name</h1>
+
         <div>
           {user ? (
             <>
+                <SearchBook books={books}/>
               <Link to="/">Home</Link>
               <Link to="/protected">Protected Page</Link>
                 <Link to="/books">Books</Link>
--- frontend/src/components/SearchBook.js
+++ frontend/src/components/SearchBook.js
@@ -4,33 +4,40 @@ import {Link} from "react-router-dom";
 
 const SearchBook = (props) => {
     const {books} = props;
-    const [searchTerm, setSearchTerm] = useState('')
+    const [query, setQuery] = useState('')
+    const [state, setstate] = useState({
+        query: '',
+        list: []
+    })
+
+    const handleChange = (e) => {
+        const results = books.filter(book => {
+            if (e.target.value === "") return books
+            return book.title.toLowerCase().includes(e.target.value.toLowerCase())
+        })
+        setstate({
+            query: e.target.value,
+            list: results
+        })
+    }
 
     return (
-        <div className='search-container'>
-            <input type="text" placeholder='Search for a title...'
-                   onChange={(e) => {
-                       setSearchTerm(e.target.value)
-                   }}/>
-            {books
-                .filter((book) => {
-                    if (book.title.toLowerCase().includes(searchTerm)) {
-                        return book
-                    } else {
-                        return ''
-                    }
-                })
-                .map((book) => (
-                        <div key={book.id}>
-                            <p>{book.title}</p>
-                            <p>{book.author}</p>
-                            <Link to={`/book/${book.id}`}>
-                                to book details
-                            </Link>
-                        </div>
-                    )
-                )
-            }
+        <div>
+            <form>
+                <input onChange={handleChange} value={state.query} type="search"/>
+            </form>
+            <ul>
+                {(state.query === '' ? "" : state.list.map(book => {
+                    // return <li key={book.id}>{book.title}</li>
+                    return (<li key={book.id}>
+                             <p>{book.title}</p>
+                             <p>{book.author}</p>
+                             <Link to={`/book/${book.id}`}>
+                                 to book details
+                             </Link>
+                         </li>)
+                }))}
+            </ul>
         </div>
     )
 }

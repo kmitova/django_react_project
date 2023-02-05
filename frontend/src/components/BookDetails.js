c802887a2fc58655bc3fe48b6d8c323621457cdb
kmitova <96522051+kmitova@users.noreply.github.com>
2023-02-05 14:32:26 +0200

    refactoring book details into smaller components

--- frontend/src/components/BookDetails.js
+++ frontend/src/components/BookDetails.js
@@ -2,21 +2,18 @@ import React, {useContext, useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 import getAccessToken from "../utils/getToken";
 import axios from "axios";
-import {URL as url, URL} from "../utils/url";
+import {URL} from "../utils/url";
 import ReviewSection from "./reviews/ReviewSection";
 import AuthContext from "../context/AuthContext";
+import WantToRead from "./WantToRead";
+import ChangeReadStatus from "./ChangeReadStatus";
+
 
 
 const BookDetails = () => {
     const {id} = useParams()
     const {user} = useContext(AuthContext);
-    console.log(user)
     const [book, setBook] = useState({});
-    const [bookIsRead, setBooksIsRead] = useState(false)
-    const [wantToRead, setWantToRead] = useState(false)
-    const [wantToReadObject, setWantToReadObject] = useState(null)
-    const [changeWantToRead, setChangeWantToRead] = useState(false)
-
 
     useEffect(() => {
         const fetchBook = async () => {
@@ -37,166 +34,17 @@ const BookDetails = () => {
             .catch(console.error);
     }, [id]);
 
-    useEffect(() => {
-        const fetchBookStatus = async () => {
-            let token = getAccessToken();
-            let result = await axios.get(`${URL}api_books/view_book_status/${id}/`, {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer  ${token}`
-                }
-            });
-            if (result.status === 200) {
-                console.log(result.data)
-                if (result.data.length > 0) {
-                    let relevantData = result.data[result.data.length-1]
-                    console.log(relevantData)
-                    setBooksIsRead(prevState => relevantData.is_read)
-                }
-                // let relevantData = result.data[result.data.length-1]
-                // console.log(relevantData)
-                // setBooksIsRead(prevState => relevantData.is_read)
-                // setBooksIsRead(result.data)
-                // setBookIsRead(result.data)
-            }
-        };
-        fetchBookStatus()
-            .catch(console.error);
-    }, [id]);
-
-    useEffect(() => {
-        const fetchWantToReadStatus = async () => {
-
-            let token = getAccessToken();
-            let result = await axios.get(`${URL}api_books/view_want_to_read/`, {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer  ${token}`
-                }
-            });
-            if (result.status === 200) {
-                console.log(result.data)
-                for (let item of result.data) {
-                    console.log(item)
-                    console.log(book)
-                    console.log(item.book, book.id)
-                    if (item.book.id === book.id) {
-                        setWantToRead(item.want_to_read)
-                        setWantToReadObject(prevState => item)
-                        console.log(item)
-
-                        break
-                    }
-                }
-                console.log(wantToReadObject)
-            }
-        };
-        fetchWantToReadStatus()
-            .catch(console.error);
-    },  [book]);
-
-
-    const changeStatus = async (e, book) => {
-        e.preventDefault();
-        setBooksIsRead(e.target.checked)
-        let token = getAccessToken()
-        let data = {
-            book: book.id,
-            user: user.user_id,
-            is_read: bookIsRead
-        }
-        console.log(data)
-        await axios.post(`${URL}api_books/change_book_status/${book.id}/`,
-            data, {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                }
-            })
-            .then((result) => {
-                console.log(result.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-    }
-
-    const handleWantToRead = async (e) => {
-        e.preventDefault()
-        console.log(book)
-        let token = getAccessToken();
-        let data = {
-            user: user.user_id,
-            book: book.id,
-            want_to_read: changeWantToRead
-        }
-        console.log(data)
-        console.log(wantToReadObject)
-        if (wantToReadObject !== null) {
-            await axios.delete(`${url}api_books/edit_want_to_read/${wantToReadObject.id}/`,
-             {
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                } , data
-            })
-            .then((result) => {
-                console.log('success, delete request')
-                console.log(result.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-        } else {
-            await axios.post(`${url}api_books/want_to_read/`,
-                data, {
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${token}`
-                    }
-                })
-                .then((result) => {
-                    console.log('success, post request')
-                    console.log(result.data);
-                })
-                .catch((error) => {
-                    console.log(error);
-                })
-        }
-    }
-
     return (
         <div>
             <h1>Book details page</h1>
             <h2>{book.title}</h2>
             <h3>{book.author}</h3>
-            <form action="" onSubmit={handleWantToRead}>
-                <button onClick={() => {
-                    setChangeWantToRead(!changeWantToRead)
-                    console.log(changeWantToRead)
-                }
-                }>{wantToRead ? "Remove from want to read" : "Add to want to read"}</button>
-                {/*{wantToRead ? <button>Remove from want to read</button> : <button>Add to want to read</button>}*/}
-            </form>
-            <form action="">
-                <label htmlFor="">Book is read</label>
-                {bookIsRead ?
-                    <input type="checkbox" name="is_read" checked
-                           onChange={(e) => changeStatus(e, book)}/> :
-                    <input type="checkbox" name="is_read" onChange={(e) => changeStatus(e, book)}/>}
-            {/*  add submit button  */}
-            </form>
+            <WantToRead book={book} user={user}/>
+            <ChangeReadStatus book={book} user={user} id={id}/>
             <ReviewSection book={book}/>
         </div>
 
     )
 }
 
-
 export default BookDetails;
-// comment
---
+++ frontend/src/components/ChangeReadStatus.js
@@ -0,0 +1,70 @@
+import React, {useEffect, useState} from "react";
+import getAccessToken from "../utils/getToken";
+import axios from "axios";
+import {URL} from "../utils/url";
+
+const ChangeReadStatus = (props) => {
+    const {book, user, id} = props;
+
+    const [bookIsRead, setBooksIsRead] = useState(false)
+
+    useEffect(() => {
+        const fetchBookStatus = async () => {
+            let token = getAccessToken();
+            let result = await axios.get(`${URL}api_books/view_book_status/${id}/`, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer  ${token}`
+                }
+            });
+            if (result.status === 200) {
+                if (result.data.length > 0) {
+                    let relevantData = result.data[result.data.length - 1]
+                    console.log(relevantData)
+                    setBooksIsRead(prevState => relevantData.is_read)
+                }
+            }
+        };
+        fetchBookStatus()
+            .catch(console.error);
+    }, [id]);
+
+    const changeStatus = async (e, book) => {
+        e.preventDefault();
+        setBooksIsRead(e.target.checked)
+        let token = getAccessToken()
+        let data = {
+            book: book.id,
+            user: user.user_id,
+            is_read: bookIsRead
+        }
+        console.log(data)
+        await axios.post(`${URL}api_books/change_book_status/${book.id}/`,
+            data, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .then((result) => {
+                console.log(result.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+    return (
+        <form action="">
+            <label htmlFor="">Book is read</label>
+            {bookIsRead ?
+                <input type="checkbox" name="is_read" checked
+                       onChange={(e) => changeStatus(e, book)}/> :
+                <input type="checkbox" name="is_read" onChange={(e) => changeStatus(e, book)}/>}
+            {/*  add submit button  */}
+        </form>
+    )
+}
+
+export default ChangeReadStatus;
